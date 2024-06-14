bcfddc6cf3c57644b22bde8ef6af4d91fcbbe2b5
memento <claude.rc@gmail.com>
2024-06-14 17:45:28 -0500

    FEAT: v2.11.1 + use conda as bootstrap

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,56 +1,60 @@
 @echo off
 setlocal EnableDelayedExpansion
 
-set "version=%PKG_VERSION%"
-set "bin_dir=%PREFIX%\Scripts"
-set "lein_file=%RECIPE_DIR%\scripts\lein.bat"
+call mamba -y leiningen
+call :bootstrap_leiningen
+call :prepare_licenses
 
-set "temp_file=lein_temp.bat"
+call :build_uberjar
 
-for /f "delims=" %%i in (%lein_file%) do (
-    set "line=%%i"
-    if "!line:~0,13!"=="set LEIN_VERSION" (
-        echo set LEIN_VERSION=%PKG_VERSION%>> "%temp_file%"
-    ) else (
-        echo %%i>> "%temp_file%"
-    )
-)
+call :install_leiningen "%SRC_DIR%\leiningen-src\target" "%PREFIX%"
+call :install_conda_scripts
 
-move /Y "%temp_file%" "%lein_file%"
-if errorlevel 1 exit 1
+goto :EOF
 
-set LIBEXEC_DIR=%PREFIX%\lib\leiningen\libexec
-mkdir %LIBEXEC_DIR%
-copy %SRC_DIR%\leiningen-jar\leiningen-%PKG_VERSION%-standalone.jar %LIBEXEC_DIR%\leiningen-%PKG_VERSION%-standalone.jar > nul
-if errorlevel 1 exit 1
-echo copied :PREFIX:\lib\leiningen\libexec\leiningen-%PKG_VERSION%-standalone.jar
-
-mkdir %PREFIX%\Scripts
-copy %RECIPE_DIR%\scripts\lein.bat %PREFIX%\Scripts\lein.bat > nul
-if errorlevel 1 exit 1
-echo copied :PREFIX:\Scripts\lein.bat
-
-set ACTIVATE_DIR=%PREFIX%\etc\conda\activate.d
-mkdir %ACTIVATE_DIR%
-copy %RECIPE_DIR%\scripts\activate.bat %ACTIVATE_DIR%\lein-activate.bat > nul
-if errorlevel 1 exit 1
-echo copied :ACTIVATE_DIR:\lein-activate.bat
-
-:: At this point we have a working Leiningen
-:: We rebuild from source to add the THIRD-PARTY.txt file
-cd "%SRC_DIR%"\leiningen-src\leiningen-core
+:: --- Functions ---
+:bootstrap_leiningen
+cd "%SRC_DIR%"\leiningen-src
   echo "Bootstrapping ...
-  set "LEIN_JAR=%PREFIX%\lib\leiningen\libexec\leiningen-%PKG_VERSION%-standalone.jar"
-  call lein bootstrap
+  call lein bootstrap > nul
   if errorlevel 1 exit 1
   echo "Third party licenses ...
   call mvn license:add-third-party -Dlicense.thirdPartyFile=THIRD-PARTY.txt > nul
   if errorlevel 1 exit 1
-  copy target\generated-sources\license\THIRD-PARTY.txt "%RECIPE_DIR%"\THIRD-PARTY.txt > nul
+  cd %SRC_DIR%
+goto :EOF
 
+:build_uberjar
 cd "%SRC_DIR%"\leiningen-src
   echo "Uberjar ...
-  call bin\lein uberjar
+  call bin\lein uberjar > nul
   if errorlevel 1 exit 1
-  echo "Update standalone jar ...
-  install -m644 %SRC_DIR%\leiningen-src\target\leiningen-"%PKG_VERSION%"-standalone.jar %LIBEXEC_DIR%\leiningen-%PKG_VERSION%-standalone.jar
+  cd %SRC_DIR%
+goto :EOF
+
+:install_leiningen
+set "_TARGET=%~1"
+set "_PREFIX=%~2"
+
+mkdir %_PREFIX%\lib
+mkdir %_PREFIX%\Scripts
+
+set "LIBEXEC_DIR=%_PREFIX%\lib\leiningen\libexec"
+mkdir %LIBEXEC_DIR%
+
+copy %RECIPE_DIR%\scripts\lein.bat %PREFIX%\Scripts\lein.bat > nul
+if errorlevel 1 exit 1
+
+install -m644 %_TARGET%\leiningen-*-standalone.jar %LIBEXEC_DIR%
+goto :EOF
+
+:install_conda_scripts
+mkdir %PREFIX%\etc\conda\activate.d
+copy %RECIPE_DIR%\scripts\activate.bat %ACTIVATE_DIR%\lein-activate.bat > nul
+if errorlevel 1 exit 1
+goto :EOF
+
+:prepare_licenses
+copy %SRC_DIR%\leiningen-src\COPYING %RECIPE_DIR%\COPYING > nul
+copy %SRC_DIR%\leiningen-src\leiningen-core\target\generated-sources\license\THIRD-PARTY.txt %RECIPE_DIR%\THIRD-PARTY.txt > nul
+goto :EOF
--- recipe/build.sh
+++ recipe/build.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 set -ex
 
-# Modify Leiningen lein-pkg installation script
-
 install_leiningen() {
+  # Installs a leiningen package
+  # $1: Installation PREFIX, $2: leiningen package path, $3: leiningen version
+  # This is a modified install script from the leiningen package
   local _prefix=$1
   local _leiningen_pkg_dir=$2
-  local _version=$3
 
   lib_dir="${_prefix}/lib"
   bin_dir="${_prefix}/bin"
@@ -15,25 +15,28 @@ install_leiningen() {
 
   mkdir -p "${bin_dir}" "${leiningen_lib_dir}/libexec"
 
-  install -m644 "${_leiningen_pkg_dir}"/leiningen-"${_version}"-standalone.jar "${leiningen_lib_dir}/libexec"
+  install -m644 "${_leiningen_pkg_dir}"/leiningen-*-standalone.jar "${leiningen_lib_dir}/libexec"
   install -m755 "${SRC_DIR}"/leiningen-src/bin/lein-pkg "${bin_dir}/lein"
 
-  sed -i -e 's@LEIN_VERSION=.*@LEIN_VERSION='"${_version}"'@g' "${bin_dir}"/lein
   sed -i -e 's@/usr/share/java@\${CONDA_PREFIX}/lib/leiningen/libexec@g' "${bin_dir}"/lein
 }
 
-# --- Installation ---
-install_leiningen "${PREFIX}" "${SRC_DIR}"/leiningen-jar "${PKG_VERSION}"
+# --- Main ---
 
-# At this point we have a working Leiningen
-# We rebuild from source to add the THIRD-PARTY.txt file
+# Install the latest conda version of leiningen
+mamba install -y leiningen
+
+# Create bootstrap from source and extract the THIRD-PARTY.txt file
 cd "${SRC_DIR}"/leiningen-src/leiningen-core
-  lein bootstrap
-  mvn license:add-third-party -Dlicense.thirdPartyFile=THIRD-PARTY.txt
-  cp target/generated-sources/license/THIRD-PARTY.txt "${RECIPE_DIR}"/THIRD-PARTY.txt
+  lein bootstrap > _lein_bootstrap.log 2>&1
+  mvn license:add-third-party -Dlicense.thirdPartyFile=THIRD-PARTY.txt > _lein_license.log 2>&1
+cd "${SRC_DIR}"
 
-cd "${SRC_DIR}"/leiningen-src
-  # Some tests fail, but we don't care for now
-  bin/lein test > _lein_test.log || true
-  bin/lein uberjar
-  install -m644 target/leiningen-"${PKG_VERSION}"-standalone.jar "${leiningen_lib_dir}/libexec"
+# Copy the license files to the recipe directory
+cp "${SRC_DIR}"/leiningen-src/leiningen-core/target/generated-sources/license/THIRD-PARTY.txt "${RECIPE_DIR}"/THIRD-PARTY.txt
+cp "${SRC_DIR}"/leiningen-src/COPYING "${RECIPE_DIR}"/COPYING
+
+# Use bootstrap to build the leiningen package
+cd "${SRC_DIR}"/leiningen-src && bin/lein uberjar && cd "${SRC_DIR}"
+
+install_leiningen "${PREFIX}" "${SRC_DIR}"/leiningen-src/target "${PKG_VERSION}"
