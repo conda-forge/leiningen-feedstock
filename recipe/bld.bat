f115008f7a8e9467a1e628d78ae6cdd016cbcfad
memento <claude.rc@gmail.com>
2024-06-16 17:34:03 -0500

    (fix) various typos

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -2,53 +2,57 @@
 setlocal EnableDelayedExpansion
 
 :: call mamba install -y leiningen
-call :install_leiningen "%SRC_DIR%\leiningen-jar" "%BUILD_PREFIX%"
-set "PATH=%BUILD_PREFIX%\Scripts;%BUILD_PREFIX%\bin;%PATH%"
-call :bootstrap_leiningen
+set "_boot-prefix=%SRC_DIR%\_conda-bootstrap"
+call :install_leiningen "%SRC_DIR%\leiningen-jar" "%_boot-prefix%"
+set "PATH=%_boot-prefix%\Scripts;%PATH%"
+call :bootstrap_leiningen %_boot-prefix%
 call :prepare_licenses
 
 call :build_uberjar
 
 call :install_leiningen "%SRC_DIR%\leiningen-src\target" "%PREFIX%"
-call :install_conda_scripts
+call :install_conda_scripts "%PREFIX%"
 
 goto :EOF
 
 :: --- Functions ---
 :bootstrap_leiningen
-cd "%SRC_DIR%"\leiningen-src
-  echo "Bootstrapping ...
-  set "LEIN_JAR=%BUILD_PREFIX%\lib\leiningen\libexec\leiningen-%PKG_VERSION%-standalone.jar"
+set "_prefix=%~1"
+cd "%SRC_DIR%"\leiningen-src\leiningen-core
+  echo "Bootstrapping ..."
+  set "LEIN_JAR=%_prefix%\lib\leiningen\libexec\leiningen-%PKG_VERSION%-standalone.jar"
   call lein bootstrap > nul
   if errorlevel 1 exit 1
-  echo "Third party licenses ...
+  echo "Third party licenses ..."
   call mvn license:add-third-party -Dlicense.thirdPartyFile=THIRD-PARTY.txt > nul
   if errorlevel 1 exit 1
-  cd %SRC_DIR%
+cd %SRC_DIR%
 goto :EOF
 
 :build_uberjar
 cd "%SRC_DIR%"\leiningen-src
-  echo "Uberjar ...
+  echo "Uberjar ..."
   call bin\lein uberjar > nul
   if errorlevel 1 exit 1
   cd %SRC_DIR%
 goto :EOF
 
 :install_leiningen
-set "_TARGET=%~1"
-set "_PREFIX=%~2"
+setlocal EnableDelayedExpansion
+set "_target=%~1"
+set "_prefix=%~2"
 
-mkdir %_PREFIX%\lib
-mkdir %_PREFIX%\Scripts
+mkdir %_prefix%\Scripts
+mkdir %_prefix%\lib
+mkdir %_prefix%\lib\leiningen
+mkdir %_prefix%\lib\leiningen\libexec
 
-set "LIBEXEC_DIR=%_PREFIX%\lib\leiningen\libexec"
-mkdir %LIBEXEC_DIR%
+copy %_target%\leiningen-%PKG_VERSION%-standalone.jar %_prefix%\lib\leiningen\libexec > nul
+dir %_prefix%\lib\leiningen\libexec\leiningen-%PKG_VERSION%-standalone.jar
+copy %RECIPE_DIR%\scripts\lein.bat %_prefix%\Scripts\lein.bat > nul
+dir %_prefix%\Scripts\lein.bat
 
-copy %RECIPE_DIR%\scripts\lein.bat %_PREFIX%\Scripts\lein.bat > nul
-if errorlevel 1 exit 1
-
-set "lein_file=%_PREFIX%\Scripts\lein.bat"
+set "lein_file=%_prefix%\Scripts\lein.bat"
 set "temp_file=%TEMP%\lein.bat"
 for /f "delims=" %%i in (%lein_file%) do (
     set "line=%%i"
@@ -59,14 +63,16 @@ for /f "delims=" %%i in (%lein_file%) do (
     )
 )
 move /Y "%temp_file%" "%lein_file%"
-
-install -m644 %_TARGET%\leiningen-%PKG_VERSION%-standalone.jar %LIBEXEC_DIR%
+endlocal
 goto :EOF
 
 :install_conda_scripts
-mkdir %PREFIX%\etc\conda\activate.d
-copy %RECIPE_DIR%\scripts\activate.bat %ACTIVATE_DIR%\lein-activate.bat > nul
+setlocal
+set "_prefix=%~1"
+mkdir %_prefix%\etc\conda\activate.d
+copy %RECIPE_DIR%\scripts\activate.bat %_prefix%\etc\conda\activate.d\lein-activate.bat > nul
 if errorlevel 1 exit 1
+endlocal
 goto :EOF
 
 :prepare_licenses
