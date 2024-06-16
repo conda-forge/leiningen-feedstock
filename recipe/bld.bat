af6e461bad89c2e5a8802ce8f5621435bcaaa7a0
memento <claude.rc@gmail.com>
2024-06-16 09:45:45 -0500

    (fix) silly me

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -2,7 +2,6 @@
 setlocal EnableDelayedExpansion
 
 :: call mamba install -y leiningen
-dir %SRC_DIR%\leiningen-jar
 call :install_leiningen "%SRC_DIR%\leiningen-jar" "%BUILD_PREFIX%"
 if errorlevel 1 exit 1
 set "PATH=%BUILD_PREFIX%\Scripts;%BUILD_PREFIX%\bin;%PATH%"
@@ -50,16 +49,17 @@ mkdir %LIBEXEC_DIR%
 copy %RECIPE_DIR%\scripts\lein.bat %PREFIX%\Scripts\lein.bat > nul
 if errorlevel 1 exit 1
 
-set "lein_file=%PREFIX%\Scripts\lein.bat"
+set "lein_file=%_PREFIX%\Scripts\lein.bat"
+set "temp_file=%TEMP%\lein.bat"
 for /f "delims=" %%i in (%lein_file%) do (
     set "line=%%i"
     if "!line:~0,13!"=="set LEIN_VERSION" (
         echo set LEIN_VERSION=%PKG_VERSION%>> "%temp_file%"
     ) else (
-        echo %%i>> "_%lein_file%"
+        echo %%i>> "%temp_file%"
     )
 )
-move /Y "_%lein_file%" "%lein_file%"
+move /Y "%temp_file%" "%lein_file%"
 
 install -m644 %_TARGET%\leiningen-%PKG_VERSION%-standalone.jar %LIBEXEC_DIR%
 goto :EOF
